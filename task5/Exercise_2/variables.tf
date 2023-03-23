4a3c7cd081f8d102bbb817c5ca8a9230769b0e4c
Phuong Bui <buiphuongaptutc@gmail.com>
2023-03-23 17:31:03 +0700

    exercise 2

--- task5/Exercise_2/variables.tf
+++ task5/Exercise_2/variables.tf
@@ -1,1 +1,18 @@
 # TODO: Define the variable for aws_region
+variable "aws_region" {
+  type        = string
+  description = "The region where the lambda function will be deployed."
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "The profile is used to deploy the infrastructure."
+  default     = "udacity"
+}
+
+variable "my_function_name" {
+  type        = string
+  description = "The name of function."
+  default     = "greet_lambda"
+}
